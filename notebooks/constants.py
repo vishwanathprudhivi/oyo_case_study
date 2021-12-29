d3540cd3701d2272304d8cd24b115f14a14e54db
vishwanathprudhivi <vishwanath.prudhivi@gmail.com>
2021-12-29 19:33:51 +0530

    oyo_data_analysis

---
+++ notebooks/constants.py
@@ -0,0 +1,3 @@
+TRAIN_DIR = r'/Users/vishwanathprudhivi/Desktop/Work/Interview/OYO/oyo_case_study/data/SampleCaseStudyData.csv'
+TRAIN_REPORT_PATH = r'/Users/vishwanathprudhivi/Desktop/Work/Interview/OYO/oyo_case_study/reports/TrainReport.html'
+ARTIFACTS_PATH = r'/Users/vishwanathprudhivi/Desktop/Work/Interview/OYO/oyo_case_study/artifacts/'
